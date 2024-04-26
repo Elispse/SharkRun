06b7afe89b10f059da2f4041668c102b521f5ab1
Elijah Kelting <elijahakbusiness@gmail.com>
2024-04-26 16:38:15 -0600

    update

---
+++ Assets/Scripts/CoralMovement.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoralMovement : MonoBehaviour
+{
+    [SerializeField] GameObject coral;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //coral.transform.position = coral.transform.position.x + transform. ;
+    }
+}
